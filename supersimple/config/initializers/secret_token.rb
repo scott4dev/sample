7927de771910a0410f63f3ae5041bb26fb02e42c
Claudio Maccari <claudio@jitidea.net>
2010-12-30 20:52:33 +0100

    a super simple app

---
+++ supersimple/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Supersimple::Application.config.secret_token = '6732dbc6326af7586c3528accc99087e7326ae95fc29452ef2760a185e24f96f45760711d7648205836634f1110a3e84f83f9dc08fce1cf1a9d73526743c33d4'
